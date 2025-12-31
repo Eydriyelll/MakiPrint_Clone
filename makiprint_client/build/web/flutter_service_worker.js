99b428bc8fb0ec3b7e35bf2fe1c62e583e53a24c
mjfgnilo <mjfgnilo@users.noreply.github.com>
2025-12-31 16:52:04 +0800

    merged mj/landing_page branch then updated build.

--- makiprint_client/build/web/flutter_service_worker.js
+++ makiprint_client/build/web/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "b161099a8633099289f2e0c9f8eb6f3a",
+"flutter_bootstrap.js": "ea98b6d6cf00a14298ac116445c3dfed",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
