75bdbe0b96ad9f9a26da0473cdc65625ec428676
mjfgnilo <mjfgnilo@users.noreply.github.com>
2026-01-01 09:50:22 +0800

    about us page + updated build

--- makiprint_client/build/web/flutter_service_worker.js
+++ makiprint_client/build/web/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "e5a5f250b29861b778108d7c89e7317d",
+"flutter_bootstrap.js": "debcf5d43c2c5f18b4ae94465356827e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
